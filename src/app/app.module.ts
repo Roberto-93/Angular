6b41f5fc3772359267a8a506cbfb58947c3f03e7
Roberto-93 <roberto.carfi@stevejobs.academy>
2023-06-09 09:55:04 +0200

    primo commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { CharacterDetailComponent } from './character-detail/character-detail.component';
+import { FavoritePageComponent } from './favorite-page/favorite-page.component';
+import { FooterComponent } from './footer/footer.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    CharacterDetailComponent,
+    FavoritePageComponent,
+    FooterComponent,
+    NavbarComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
