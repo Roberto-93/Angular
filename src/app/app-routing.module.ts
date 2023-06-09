b4678784517489f6f204ce500f8cdf0516077266
LorenaFrazz <frazzetta.lorena@hotmail.com>
2023-06-09 13:04:41 +0200

    first commit listing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import {LoginComponent} from './login/login.component';
 import { HomeComponent } from './home/home.component';
+import { ListingPageComponent } from './listing-page/listing-page.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
@@ -15,8 +16,9 @@ const routes: Routes = [
       { path: 'sezione1', component: HomeComponent },
       { path: 'sezione2', component: HomeComponent },
 
-    ]
-  },
+]
+},
+{path: 'listing', component: ListingPageComponent},
   
 ];
 
--- src/app/listing-page/listing-page.component.ts
+++ src/app/listing-page/listing-page.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { throwError } from 'rxjs';
+import { catchError } from 'rxjs/operators';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-listing-page',
   templateUrl: './listing-page.component.html',
   styleUrls: ['./listing-page.component.scss']
 })
-export class ListingPageComponent {
+export class ListingPageComponent implements OnInit {
+  characters: any[] = [];
+  errorMessage: string = '';
+  searchQuery: string = '';
+  filteredCharacters: any[] = [];
+  isImageHovered: boolean = false;
+
+  constructor(
+    private http: HttpClient,
+    private router: Router, 
+    ) { }
+
+  // ngOnInit() {
+  //   this.fetchCharacters();
+  // }
+
+  ngOnInit() {
+    const user = localStorage.getItem('user');
+    if (!user) {
+      this.router.navigate(['/404']);
+    } else {
+      this.fetchCharacters();
+      // this.filteredCharacters = this.characters;
+      }
+    }
+
+  fetchCharacters() {
+    const url = 'https://rickandmortyapi.com/api/character';
+    this.http.get<any>(url).pipe(
+      catchError(error => {
+        this.errorMessage = 'Error occurred while fetching the characters';
+        return throwError(() => new Error(error));
+      })
+    ).subscribe(response => {
+      this.characters = response.results;
+      this.filterCharacters();
+    });
+  }
+
+  filterCharacters() {
+    this.filteredCharacters = this.characters.filter((character: { name: string; }) => {
+      return character.name.toLowerCase().includes(this.searchQuery.toLowerCase());
+    });
+  }
+
+  onSearchInputChange(event: Event) {
+    const inputElement = event.target as HTMLInputElement;
+    this.searchQuery = inputElement.value;
+    this.filterCharacters();
+  }
+
+  onImageMouseEnter(character: any) {
+    character.isImageHovered = true;
+  }
+
+  onImageMouseLeave(character: any) {
+    character.isImageHovered = false;
+  }
 
 }
+
